d4228081c1328b0fcd217e9079dc18c4a2cdf22e
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-22 21:28:58 +0100

    Aggiungere i file di progetto.

---
+++ RadioClasseCasa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioClasseCasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio r = new Radio();
+            r.Volume = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Volume aumentato del 10/100:{0}, Volume diminuito del 10/100:{1}", r.Aumentav(), r.Diminuisciv());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RadioClasseCasa/Radio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioClasseCasa
+{
+    internal class Radio
+    {
+        public float Volume { get; set; }
+
+        public Radio()
+        {
+        }
+        
+        public float Aumentav()
+        {
+            return (float)(Volume + 0.1 * Volume);
+        }
+
+        public float Diminuisciv()
+        {
+            return (float)(Volume - 0.1 * Volume);
+        }
+
+    }
+}
